cec0d2471a3b0e10d85326b64a49c21b502212b8
Shawn Wang <shawn2025@outlook.com>
2024-12-14 12:06:23 +0100

    day13 done

--- day13/src/App.java
+++ day13/src/App.java
@@ -5,31 +5,27 @@ public class App {
     public static void main(String[] args) throws Exception {
         File file = new File("..\\lib\\input.txt");   
         Scanner scanbutton = new Scanner(file).useDelimiter("[^0-9]+");
-        int answer = 0;
+        double answer = 0;
         while(scanbutton.hasNext())
         {
-            int ax = scanbutton.nextInt();
-            int ay = scanbutton.nextInt();
-            int bx = scanbutton.nextInt();
-            int by = scanbutton.nextInt();
-            int goalx = scanbutton.nextInt();
-            int goaly = scanbutton.nextInt();
-            System.out.println(ax);
-            System.out.println(ay);
-            System.out.println(bx);
-            System.out.println(by);
-            System.out.println(goalx);
-            System.out.println(goaly);
+            double ax = scanbutton.nextDouble();
+            double ay = scanbutton.nextDouble();
+            double bx = scanbutton.nextDouble();
+            double by = scanbutton.nextDouble();
+            double goalx = scanbutton.nextDouble();
+            double goaly = scanbutton.nextDouble();
+            
+            // goalx += 10000000000000.0;
+            // goaly += 10000000000000.0;
 
-            int bpresses = Math.min(goaly / by, goalx / bx);
-            int apresses = 0;
-            int currentx = bpresses * bx + apresses * ax;
-            int currenty = bpresses * by + apresses * ay;
-            apresses = Math.min(goaly - currenty  / ay, goalx / bx);
-            if(currentx == goalx && currenty == goaly)
+            double b = (goalx * ay - ax * goaly)/(-by * ax + bx * ay);
+            double a = (goaly - by * b)/ ay;
+            if(a % 1 == 0 && b % 1 == 0)
             {
-                answer = answer + bpresses + apresses * 3;
+                answer = answer + b + a * 3;
             }
+            
         }
+        System.out.println(answer);
     }
 }
